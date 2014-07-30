4fea6bea56b7f9bf239d0b820c694bc2114a89aa
Summer <summermichiko@gmail.com>
2014-07-30 14:00:57 -0400

    added final solutions

--- functional.js
+++ functional.js
@@ -1,142 +1,117 @@
-function countWords(sentence) { //1
-	var total = 0;
-	for (var i = 0; i < sentence.length; i++) {
-		if (sentence[i] == " ") {
-			total++;
-		}
-	}
-	return total + 1;
-};
+function countWords(sentence) {
+  return sentence.split(" ").length;
+}
+  
+function makeAdder(num1) {
+  // this variable will be "closed upon" aka, it will be kept alive
+  // num1 = 2 
+  var adderof2Function = function (num2) {
+    return num1+num2;
+  };
+  return adderof2Function;
+}
 
+function forEach(array, funcRunOnEachElement) {
+  for (var i = 0; i < array.length; i++) {
+    funcRunOnEachElement(array[i]);
+  }
+}
 
-function makeAdder(adder) { //2,3
-	return function (secondAdder) {
-		return adder + secondAdder;
-	}
-};
+function map(array, funcRunOnEachElement) {
+  var newMapArray = [];
+  for(var i =0; i<array.length; i++){
+    newMapArray.push(func(arr[i]))
+  }
+  return newMapArray;
+}
 
-
-function forEach(array, action) {
-  for (var i = 0; i < array.length; i++)
-    action(array[i]);
-};
-
-
-function map() { //5, 6 (6 not passing)
-	var numbers = [1,2,3];
-	var doubles = numbers.map(function(numbers){
-		return numbers * 2;
-	});
-	return doubles;
-	var triples = numbers.map(function(numbers){
-		return numbers * 3;
-	});
-	return triples; //failing to return this value (test 6)
-};
-
-
-function filter() {//7, 8 (8 not passing)
-    var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-    var even_filter = []; 
-	var odd_filter = [];
-	for(i = 0; i < numbers.length; i++) {
-		if(numbers[i] % 2 === 0){
-			even_filter.push(numbers[i]);
-		} else {
-			odd_filter.push(numbers[i]);
-		}
-	}
-	return even_filter;
-	return odd_filter; //failing to return this value (test 8)
-};
-
-
-function contains(a,b) { //9, 10 (9 not passing)
-	for(i = 0; i < a.length; i++){
-		if(a[i] === b) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-};
-
-
-//tests 11, 12, 13 (12 not passing)
-function reduce(array, startPoint, combiningFunc) {
-    var current = null;
-    for(var i = startPoint; i<array.length; i++){
-        current = combiningFunc(current, array[i]);
+function filter(array, filter_func) {
+  var returnArray = [];
+  forEach(array, function(el) {
+    if (filter_func(el)) {
+      returnArray.push(el);
     }
-    return current;
-};
-var countWordsInReduce = function() {
-    var newString = this.join(" ");
-    return newString.split(" ").length;
-};
+  });
+  return returnArray;
+}
+
+function contains(arrayOrObj, searchValue) {
+  for (key in arrayOrObj) {
+    if (arrayOrObj.hasOwnProperty(key)) {
+      if (arrayOrObj[key] === searchValue) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function reduce(array, startVal, interatingFunction) {
+  var currentVal = startVal;
+  
+  forEach(array, function(num) {
+    currentVal = interatingFunction(currentVal, num)
+  });
+  return currentVal;
+}
+
+function countWordsInReduce(currentVal, wordsArray) {
+  return currentVal + countWords(wordsArray);
+}
+
 function sum(array) {
-    return reduce(array, 0, function(a, b) {
-    	return a+b;
-    });
-};
+  var addFunc = function(currVal, newVal) {
+    return currVal + newVal;
+  };
+  return reduce(array, 0, addFunc);
+}
 
+// cool version
+function every(array, iteratingFunction) {
+  return reduce(array, true, function(currVal, newVal) {
+    return currVal && iteratingFunction(newVal);
+  });
+}
 
-function every(array, test) { //14 - 18
-	var pass = true;
-	for(i = 0; i < array.length; i++) {
-		if((array[i] === false) || (test(array[i]) === false)) {
-			pass = false;
-		}
-	}
-	return pass;
+// clear version
+function every(array, iteratingFunction) {
+  for(var i=0; i<array.length; i++) {
+    if (!iteratingFunction(array[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// cool version 2.0
+function any(array, iteratingFunction) {
+  if (typeof iteratingFunction === "undefined") {
+    iteratingFunction = function(item) {
+      return item;
+    }
+  }
+  return reduce(array, false, function(currVal, newVal) {
+    var returnBool = currVal || iteratingFunction(newVal);
+    return returnBool;
+  });
+}
+
+function once(inputFunc) {
+  var calledAlready = false;
+  return function() {
+    if (!calledAlready) {
+      inputFunc();
+      calledAlready = true;
+    }
+  };
 }
 
 
-function any(array, func){ //19, 20, 21, 22, 23
-  	if(func === undefined){
-   		var func = function(item){
-    		return item;
-  		};
-	};
- 	for(var i=0; i<array.length; i++){
-    	if(func(array[i])){
-      		return true;
-    	}
-  	};
-  	return false;
-};
-
-
-function once() { //24, first attempt
-    var num = 0; //how to incorporate increment()?
-		do {
-			num++;
-		}
-		while (num > 2);
-        return num;
-};
-
-var once = function (func) {
-	var ran = false;
-	return function () {
-		if (!ran) {func();}
-		ran = true;
-	};
-};
-
-
-function wrapper(func, wrapperFunc) { //25 not passing
-	return function(newFunction) {
-		//give it the block function
-		wrapperFunc(wrapper);
-	};
-};
-
-function wrapper(fun, wrap) {
-	return function() {
-		return wrap(fun);
-	}
-};
+function wrapper(codeBlockFunction, blockWrapFunc) {
+  return function() {
+    return blockWrapFunc(codeBlockFunction);
+  }
+}
 
 
 
